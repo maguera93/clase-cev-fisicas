103678449774604381ad3d668bdfd838a1780884
Jose Antonio Andujar Clavell <jandujar@cev.com>
2020-02-27 17:14:14 +0100

    overlap

---
+++ Cube 2.5D/Assets/Scripts/OverlapTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlapTest : MonoBehaviour
+{
+    Collider[] hits;
+    public LayerMask layer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        hits = Physics.OverlapBox(transform.position, Vector3.one, 
+            Quaternion.identity, layer);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            Debug.Log(hits[i].name);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, Vector3.one * 2);
+    }
+}
---
+++ Cube 2.5D/Assets/Scripts/PhysicsTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsTest : MonoBehaviour
+{
+    Ray ray; // rayo
+    RaycastHit hit; // objeto con el que ha chocado el rayo
+    public LayerMask layer; // capa de lo que choca con el rayo
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        int sign = 1;
+
+        for (int i = 0; i < 3; i++)
+        {
+            pos.x += 0.5f * i * sign;
+            sign *= -1;
+
+            ray = new Ray(transform.position, Vector3.down);
+            hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit, 1.1f, layer))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color32(254, 10, 251, 255);
+
+        Vector3 pos = transform.position;
+        int sign = 1;
+
+        for (int i = 0; i < 3; i++)
+        {
+            pos.x += 0.5f * i * sign;
+            sign *= -1;
+
+            Gizmos.DrawRay(pos, Vector3.down);
+        }
+    }
+}
